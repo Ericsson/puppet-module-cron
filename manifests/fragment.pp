abce560f485b84424bbf6832fe80d2aa384957c0
nbentoumi <nasredine.bentoumi@gmail.com>
2014-06-10 20:55:32 -0400

    moved resource type to separate file

---
+++ manifests/fragment.pp
@@ -0,0 +1,54 @@
+#--------------------------------------------#
+#
+# manage cron jobs in separate files
+# call with ensure_cron=> absent to delete the job
+#
+#--------------------------------------------#
+#  usage in manifest
+#  cron::cron_job_file { "file_name":
+#        ensure_cron => present,
+#        type        => "d",
+#        cron_content    => "script | cron"
+#
+#
+# usage in Hiera
+#  cron::cron_files:
+#     'file_name':
+#       ensure_cron: 'present'
+#       type: "daily"
+#       cron_content: |-
+#            #!/bin/bash
+#            # This File is managed by puppet
+#            script
+#            .
+#            .
+#            EOF
+#-------------------------------------------#
+
+define cron::fragment (
+  $ensure_cron  = 'absent',
+  $type         = 'daily',
+  $cron_content = '',
+) {
+
+  include cron
+
+  case $type {
+  'd':                                  { $cron_mode = 644 }
+  'daily','weekly','monthly','yearly':  { $cron_mode = 755 }
+  default: {fail("Valid values are d, daily, weekly, monthly, yearly")}
+  }
+
+  file { "/etc/cron.${type}/${name}":
+    ensure  => $ensure_cron,
+    content => $cron_content,
+    force   => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => $cron_mode,
+    require => Package[$cron::package_name],
+  }
+
+
+}
+
