6abdf9c22ba338af49f018232f431b9e2e16827d
nbentoumi <nasredine.bentoumi@gmail.com>
2014-06-11 08:01:47 -0400

    removed comment and will add them to README file

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,18 +19,21 @@
 
 class cron (
   #Default values
-  $enable           = true,
+  $enable_cron      = true,
+  $package_ensure   = 'present',
   $ensure_state     = 'running',
   $crontab_path     = '/etc/crontab',
-  $cron_allow       = absent,
-  $cron_deny        = absent,
+  $cron_allow       = 'absent',
+  $cron_deny        = 'absent',
   $cron_allow_path  = '/etc/cron.allow',
   $cron_deny_path   = '/etc/cron.deny',
   $cron_files       = undef,
+  $var_spool_cron   = undef,
   $cron_allow_users = undef,
   $cron_deny_users  = undef,
   $crontab_vars     = '',
   $crontab_tasks    = '',
+  
 ) {
 
   # Check the client os to define the package name and service name
@@ -41,95 +44,85 @@ class cron (
     default: {fail("Module cron does not support osfamily: ${::osfamily}")}
   }
    
+  # validation
+  if ! ($ensure_state in [ running, stopped ]) {
+     fail("cron::ensure_state is $ensure_state and must be running or stopped")
+  }
 
-  #--------------------------------------------------------------------------#
-  # manage /etc/cron.allow and /etc/cron.deny
-  # If the file cron.allow exists, only users listed in it are allowed
-  # to use cron, and the cron.deny file is ignored.
-  #
-  # If cron.allow does not exist, users listed in cron.deny are not
-  # allowed to use cron.
-  #
-  # Usage :
-  # cron::cron_allow: 'true' => enable the usage of /etc/cron.allow,
-  # and create the file (by default it is set to false)
-  #
-  # cron::cron_allow_users: (This add the users below to /etc/cron.allow)
-  #   - user1
-  #   - user2
-  #
-  # cron::cron_deny_users: (This add the users below to /etc/cron.deny)
-  #   - user1
-  #   - user2
-  #--------------------------------------------------------------------------#
-  #$cron_allow_users = hiera('cron::cron_allow_users', [])
+  if ! ($package_ensure in [ "present", "absent" ]) {
+     fail("cron::package_ensure is $package_ensure and must be absent or present")
+  }
+  if ! ($cron_allow in [ "present", "absent" ]) {
+     fail("cron::cron_allow is $cron_allow and must be absent or present")
+  }
+  if ! ($cron_deny in [ "present", "absent" ]) {
+     fail("cron::cron_deny is $cron_deny and must be absent or present")
+  }
+  case type($enable_cron) {
+   'string': { 
+    validate_re($enable_cron, '^(true|false)$', "cron::enable_cron may be either 'true' or 'false' and is set to <$enable_cron>")
+    $enable_cron_real = str2bool($enable_cron)
+    }
+   'boolean': {
+    $enable_cron_real = $enable_cron
+    }
+   default: {
+   fail('cron::enable_cron type must be true or false.')
+   }
+  }
+  if $cron_allow_users != undef {
+   validate_array($cron_allow_users)
+   $cron_allow='present'
+  } else { 
+  $cron_allow_real=$cron_allow
+  }
+  if $cron_deny_users != undef {
+   validate_array($cron_deny_users)
+   $cron_deny_real='present'
+  } else {
+  $cron_deny_real=$cron_deny
+  }
+  if $cron_files != undef {
+   create_resources(cron::fragment,$cron_files)
+  }
+  if $var_spool_cron != undef {
+   create_resources(cron::var_spool_cron,$var_spool_cron)
+  }
+  if ($cron_tasks != '') {
+   validate_hash($cron_tasks)
+  }
+  if ($cron_vars != '') {
+   validate_hash($cron_vars)
+  } 
   
-  #$cron_allow_real = $cron_allow ? {
-  #true    => 'present',
-  #default => 'absent',
-  #}
-  
-  file {'cron_allow':
-    ensure  => $cron_allow, 
+# End of validation
+
+file {'cron_allow':
+    ensure  => $cron_allow_real, 
     path    => $cron_allow_path,
     owner   => root,
     group   => root,
     mode    => '0644',
     content => template('cron/cron_allow.erb'),
     require => Package[$package_name],
-  }
+ }
 
 
-  #$cron_deny_users = hiera('cron::cron_deny_users', [])
-  #$cron_deny_real = $cron_deny ? {
-  #true    => 'present',
-  #default => 'absent',
-  #}
-  file {'cron_deny':
-    ensure  => $cron_deny,
+ file {'cron_deny':
+    ensure  => $cron_deny_real,
     path    => $cron_deny_path,
     owner   => root,
     group   => root,
     mode    => '0644',
     content => template('cron/cron_deny.erb'),
     require => Package[$package_name],
-  }
+ }
 
-  #--------------------------------------------------------------------------#
-  # get variables from hiera file
-  #
-  #  Hiera Usage:
-  #  #cron::crontab_vars:
-  #  SHELL: /bin/bash
-  #  PATH: /sbin:/bin:/usr/sbin:/usr/bin
-  #  MAILTO: root
-  #  HOME: /root
-  #
-  #  If the variable above are not provided (crontab_vars is empty),
-  #  we will use default value defined template
-  #--------------------------------------------------------------------------#
-
-  #$crontab_vars  = hiera('cron::crontab_vars', {})
-
-  #--------------------------------------------------------------------------#
-  # get the tasks from hiera
-  #
-  # Hiera usage
-  #
-  # cron::crontab_tasks:
-  #        'cronjob_task':
-  #     - "* * * * * root 'cron_command'"
-  #    'cronjob_task':
-  #     - "* * * * * root 'cron_command'"
-  #
-  #--------------------------------------------------------------------------#
-
-  #$crontab_tasks = hiera('cron::crontab_tasks', {})
 
 package {'cron':
-    ensure      => 'present',
+    ensure      => $package_ensure,
     name        => $package_name,
-  }
+ } 
 
 file {'crontab':
     ensure  => present,
@@ -139,76 +132,17 @@ file {'crontab':
     mode    => '0644',
     content => template('cron/crontab.erb'),
     require => Package[$package_name],
-}
+ }
 
 
   service {'cron':
     ensure      => $ensure_state,
-    enable      => $enable,
+    enable      => $enable_cron_real,
     name        => $service_name,
     require     => File[$crontab_path],
     subscribe   => File['crontab'],
 
   }
-
-
-#------------------------------------------------#
-# puppet has already cron resource defined
-# and below manage cron in /var/spool/cron
-#-----------------------------------------------#
-#  Usage in manifest (example):
-#   cron::cron_spool {"cron_name":
-#       ensure =>present,
-#       command => 'command to run',
-#       minute  => ['0', '15', '30', '45']
-#   }
-#
-#  Usage with Hiera (example):
-#  cron::var_spool_cron:
-#     cron_name:
-#      ensure: 'present'
-#      command: 'command to run'
-#      minute: [0, 15, 30, 45]
-#-----------------------------------------------#
-
-
-
-#define cron_spool (
-#  $ensure   = 'absent',
-#  $target   = 'root',
-#  $user     = 'root',
-#  $command  = '',
-#  $minute   = '*',
-#  $hour     = '*',
-#  $monthday = '*',
-#  $month    = '*',
-#  $weekday  = '*',
-#) {
-#  cron { $name:
-#    ensure   => $ensure,
-#    command  => $command,
-#    minute   => $minute,
-#    hour     => $hour,
-#    monthday => $monthday,
-#    month    => $month,
-#    weekday  => $weekday,
-#    target   => $target,
-#    user     => $user,
-#  }
-#}# End of cron_spool
-
-# below create cron entry from hiera hash as described above
-
-#$cron_spool = hiera('cron::var_spool_cron', {})
-#create_resources(cron_spool,$cron_spool)
-
-
-  #$cron_files = hiera('cron::cron_files')
-  if $cron_files != undef {
-  create_resources(cron::fragment,$cron_files)
-  }
-
-
 
 }
 
