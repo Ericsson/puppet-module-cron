33f66c943e49fa67d9137549bbf22312e493648a
Phil Friderici <phil.friderici@ericsson.com>
2014-07-11 14:35:12 +0000

    style guide fixes

--- manifests/fragment.pp
+++ manifests/fragment.pp
@@ -32,15 +32,21 @@ define cron::fragment (
 ) {
 
   include cron
-  
-  if ! ($ensure_cron in [ "present", "absent" ]) {
-     fail("cron::fragment:ensure_cron is $ensure_cron and must be absent or present")
+
+  if ! ($ensure_cron in [ 'present', 'absent' ]) {
+    fail("cron::fragment:ensure_cron is ${ensure_cron} and must be absent or present")
   }
   validate_string($cron_content)
   case $type {
-  'd':                                  { $cron_mode = 0644 }
-  'daily','weekly','monthly','yearly':  { $cron_mode = 0755 }
-  default: {fail("Valid values are d, daily, weekly, monthly, yearly")}
+    'd': {
+      $cron_mode = '0644'
+    }
+    'daily','weekly','monthly','yearly': {
+      $cron_mode = '0755'
+    }
+    default: {
+      fail('Valid values are d, daily, weekly, monthly, yearly')
+    }
   }
 
   file { "/etc/cron.${type}/${name}":
--- manifests/init.pp
+++ manifests/init.pp
@@ -33,79 +33,85 @@ class cron (
   $cron_deny_users  = undef,
   $crontab_vars     = '',
   $crontab_tasks    = '',
-  
+
 ) {
 
   # Check the client os to define the package name and service name
 
   case $::osfamily {
-    ubuntu, suse: {$package_name = 'cron' $service_name = 'cron' }
-    redhat, centos: {$package_name = 'crontabs' $service_name = 'crond' }
-    default: {fail("Module cron does not support osfamily: ${::osfamily}")}
-  }
-   
-  # validation
-  if ! ($ensure_state in [ running, stopped ]) {
-     fail("cron::ensure_state is $ensure_state and must be running or stopped")
+    'Ubuntu', 'Suse': {
+      $package_name = 'cron'
+      $service_name = 'cron'
+    }
+    'RedHat', 'Centos': {
+      $package_name = 'crontabs'
+      $service_name = 'crond'
+    }
+    default: {
+      fail("cron supports RedHat, CentOS, Suse and Ubuntu. Detected osfaimly is <${::osfamily}>.")
+    }
   }
 
-  if ! ($package_ensure in [ "present", "absent" ]) {
-     fail("cron::package_ensure is $package_ensure and must be absent or present")
+# validation
+  if ! ($ensure_state in [ running, stopped ]) {
+    fail("cron::ensure_state is ${ensure_state} and must be running or stopped")
   }
-  if ! ($cron_allow in [ "present", "absent" ]) {
-     fail("cron::cron_allow is $cron_allow and must be absent or present")
+
+  if ! ($package_ensure in [ 'present', 'absent' ]) {
+    fail("cron::package_ensure is ${package_ensure} and must be absent or present")
   }
-  if ! ($cron_deny in [ "present", "absent" ]) {
-     fail("cron::cron_deny is $cron_deny and must be absent or present")
+  if ! ($cron_allow in [ 'present', 'absent' ]) {
+    fail("cron::cron_allow is ${cron_allow} and must be absent or present")
+  }
+  if ! ($cron_deny in [ 'present', 'absent' ]) {
+    fail("cron::cron_deny is ${cron_deny} and must be absent or present")
   }
   case type($enable_cron) {
-   'string': { 
-    validate_re($enable_cron, '^(true|false)$', "cron::enable_cron may be either 'true' or 'false' and is set to <$enable_cron>")
-    $enable_cron_real = str2bool($enable_cron)
+    'string': {
+      validate_re($enable_cron, '^(true|false)$', "cron::enable_cron may be either 'true' or 'false' and is set to <${enable_cron}>")
+      $enable_cron_real = str2bool($enable_cron)
     }
-   'boolean': {
-    $enable_cron_real = $enable_cron
+    'boolean': {
+      $enable_cron_real = $enable_cron
     }
-   default: {
-   fail('cron::enable_cron type must be true or false.')
-   }
+    default: {
+      fail('cron::enable_cron type must be true or false.')
+    }
   }
   if $cron_allow_users != undef {
-   validate_array($cron_allow_users)
-   $cron_allow='present'
-  } else { 
-   $cron_allow_real=$cron_allow
+    validate_array($cron_allow_users)
+    $cron_allow='present'
+  } else {
+    $cron_allow_real=$cron_allow
   }
   if $cron_deny_users != undef {
-   validate_array($cron_deny_users)
-   $cron_deny_real='present'
+    validate_array($cron_deny_users)
+    $cron_deny_real='present'
   } else {
-  $cron_deny_real=$cron_deny
+    $cron_deny_real=$cron_deny
   }
   if $cron_files != undef {
-   create_resources(cron::fragment,$cron_files)
+    create_resources(cron::fragment,$cron_files)
   }
   if ($cron_tasks != '') {
-   validate_hash($cron_tasks)
+    validate_hash($cron_tasks)
   }
   if ($cron_vars != '') {
-   validate_hash($cron_vars)
-  } 
-  
+    validate_hash($cron_vars)
+  }
 # End of validation
 
-file {'cron_allow':
-    ensure  => $cron_allow_real, 
+  file {'cron_allow':
+    ensure  => $cron_allow_real,
     path    => $cron_allow_path,
     owner   => root,
     group   => root,
     mode    => '0644',
     content => template('cron/cron_allow.erb'),
     require => Package[$package_name],
- }
+  }
 
-
- file {'cron_deny':
+  file {'cron_deny':
     ensure  => $cron_deny_real,
     path    => $cron_deny_path,
     owner   => root,
@@ -113,15 +119,14 @@ file {'cron_allow':
     mode    => '0644',
     content => template('cron/cron_deny.erb'),
     require => Package[$package_name],
- }
+  }
 
+  package {'cron':
+    ensure => $package_ensure,
+    name   => $package_name,
+  }
 
-package {'cron':
-    ensure      => $package_ensure,
-    name        => $package_name,
- } 
-
-file {'crontab':
+  file {'crontab':
     ensure  => present,
     path    => $crontab_path,
     owner   => root,
@@ -129,16 +134,14 @@ file {'crontab':
     mode    => '0644',
     content => template('cron/crontab.erb'),
     require => Package[$package_name],
- }
-
+  }
 
   service {'cron':
-    ensure      => $ensure_state,
-    enable      => $enable_cron_real,
-    name        => $service_name,
-    require     => File[$crontab_path],
-    subscribe   => File['crontab'],
-
+    ensure    => $ensure_state,
+    enable    => $enable_cron_real,
+    name      => $service_name,
+    require   => File[$crontab_path],
+    subscribe => File['crontab'],
   }
 
 }
