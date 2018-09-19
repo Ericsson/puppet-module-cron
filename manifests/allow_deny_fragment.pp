92b655704bd8e76f3424c39e9b751ab04b7597f3
Lesley Kimmel <lesley.j.kimmel.ctr@mail.mil>
2018-09-19 11:01:48 -0500

    Updated to allow for multiple 'applications' to add users to cron.allow and cron.deny via a defined type (cron::allow_deny_fragment).

---
+++ manifests/allow_deny_fragment.pp
@@ -0,0 +1,18 @@
+define cron::allow_deny_fragment (
+  $users,
+  $type,
+) {
+  validate_re($type, ['^allow$','^deny$'])
+  validate_array($users)
+
+  $target = $type ? {
+    'allow' => $cron::cron_allow_path,
+    'deny'  => $cron::cron_deny_path,
+  }
+
+  concat::fragment { $name:
+    target  => $target,
+    order   => '02',
+    content => template('cron/_cron_allow_deny_fragment.erb'),
+  }
+}
